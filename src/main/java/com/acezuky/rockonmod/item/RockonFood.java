abc17b75dc01f8f39f1f7640c815e9f958310117
rock-on <tranghk@gmail.com>
2022-08-10 12:41:40 +0700

    add rockonfood (rice)

--- src/main/java/com/acezuky/rockonmod/item/RockonFood.java
+++ src/main/java/com/acezuky/rockonmod/item/RockonFood.java
@@ -1,11 +1,24 @@
 package com.acezuky.rockonmod.item;
 
+import com.acezuky.rockonmod.RockonMod;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.Potion;
+
+import java.util.function.Supplier;
 
 public class RockonFood extends Item {
     public RockonFood() {
         super(new Item.Properties()
-                //.group(ITemGroup.)
+                .tab(RockonMod.TAB)
+                .food(new Food.Builder()
+                        .alwaysEat()
+                        .nutrition(2)
+                        .saturationMod(1.2f)
+                        .effect(new EffectInstance(Effects.SATURATION, 400,1),1)
+                        .build())
         );
     }
 }
